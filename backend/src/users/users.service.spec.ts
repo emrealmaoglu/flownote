677f5e59dd69d848973b102cc003e568dc3ab634
Yunus Emre Almaoglu <emrealmaogluu@gmail.com>
2025-12-25 12:09:17 +0300

    fix(test): restore test and testing module imports

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
 import { ConflictException, NotFoundException } from "@nestjs/common";
 import * as bcrypt from "bcrypt";
 import { UsersService } from "./users.service";
